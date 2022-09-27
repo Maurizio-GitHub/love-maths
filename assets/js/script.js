6d7dc3e782b720a54a132f50096c5597f59c8434
Maurizio <maurizio.loffredo@outlook.com>
2022-09-27 13:34:36 +0000

    Feat: added code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,8 +56,10 @@ function runGame(gameType) {
 
     if (isCorrect) {
         alert('Hey! You got it right! :D');
+        incrementScore();
     } else {
         alert(`Awwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     // Keeps running a math game of the same type, which is given by the second element of the returned calculateCorrectAnswer() array:
@@ -87,11 +89,23 @@ function calculateCorrectAnswer() {
     }
 }
 
-function incrementScore() {
+/**
+ * Gets the current score from the DOM and increments it by 1.
+ */
+ function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1.
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 
 }
 
